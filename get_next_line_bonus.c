491159acafb013c862d6ec9f097cdbdbd05b65a1
ImanSafae <86429366+ImanSafae@users.noreply.github.com>
2021-10-29 12:22:49 +0200

    Add files via upload

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 void	gnl_check_previous_rest(char **rest, char **line)
 {
